dcb2a4762a0c7cbe09f34e57aa4a2a9e4ad9fb16
Loana Albisser <loana.albisser@peax.ch>
2021-08-19 08:56:51 +0200

    add splashscreen

--- src/app/splashscreen/splashscreen.page.ts
+++ src/app/splashscreen/splashscreen.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
 import { SplashScreen } from '@capacitor/splash-screen';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-splashscreen',
@@ -9,7 +10,11 @@ import { SplashScreen } from '@capacitor/splash-screen';
   styleUrls: ['./splashscreen.page.scss'],
 })
 export class SplashscreenPage implements OnInit {
+  @ViewChild('icon ', { static: false }) icon;
+
+
   constructor(
+    private animationCtrl: AnimationController,
     private authenticationService: AuthenticationService,
     private router: Router
   ) {
@@ -19,14 +24,29 @@ export class SplashscreenPage implements OnInit {
   }
 
   ngOnInit() {
-    this.authenticationService.initializeUser().then((user) => {
-      if (user != null) {
-        this.authenticationService.currentUser = user;
-        this.router.navigate(['/home', { uid: user, replaceUrl: true }]);
-      } else {
-        this.router.navigate(['/login'], { replaceUrl: true });
-      }
-      SplashScreen.hide();
-    });
+    SplashScreen.hide();
+  }
+
+  ionViewWillEnter(){
+    const rowElement = this.icon.el;
+    const animation = this.animationCtrl.create()
+      .addElement(rowElement)
+      .duration(1000)
+      .iterations(1)
+      .keyframes([
+        { offset: 0, transform: 'scale(0.9)' },
+        { offset: 0.5,  transform: 'scale(1.2)' },
+        { offset: 1, transform: 'scale(0.9)' }
+      ]);
+      animation.play().then(() => {
+        this.authenticationService.initializeUser().then((user) => {
+          if (user != null) {
+            this.authenticationService.currentUser = user;
+            this.router.navigate(['/home', { uid: user, replaceUrl: true }]);
+          } else {
+            this.router.navigate(['/login'], { replaceUrl: true });
+          }
+        });
+      });
   }
 }
